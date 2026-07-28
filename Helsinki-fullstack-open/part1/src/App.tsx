d895fd19dab94a487e5c67c4d762d28c80477b59
Brian <Bkibagendi20@gmail.com>
2026-07-28 05:53:05 -0700

    Exercise 1.2 - Refactor content into reusable components

--- Helsinki-fullstack-open/part1/src/App.tsx
+++ Helsinki-fullstack-open/part1/src/App.tsx
@@ -1,9 +1,15 @@
-
-function Header({course}) {
+type HeaderProps = {
+  course: string
+}
+function Header({course}: HeaderProps) {
   return  <h1>{course}</h1> 
 }
 
-function Part({name, exercises}) {
+type Partprops = {
+  name: string
+  exercises: number
+}
+function Part({name, exercises}: Partprops) {
   return (
     <div>
       <p>
@@ -13,24 +19,64 @@ function Part({name, exercises}) {
   )
 }
 
+type Contentprops = {
+  part1: string
+  exercises1: number
+  part2: string
+  exercises2: number
+  part3: string
+  exercises3: number
+}
+function Content({part1, exercises1, part2, exercises2, part3, exercises3}: Contentprops) {
+  return (
+    <div>
+      <Part name={part1} exercises={exercises1} />
+      <Part name={part2} exercises={exercises2} />
+      <Part name={part3} exercises={exercises3} />
+    </div>
+  )
+}
+
+type TotalProps = {
+    exercises1: number
+    exercises2: number
+    exercises3: number
+}
+
+function Total({exercises1, exercises2, exercises3}: TotalProps) {
+  return (
+    <p>
+      number of exercises {exercises1 +exercises2 + exercises3}
+    </p>
+  )
+}
+
 function App() {
   const course = "Half stack application development"
   return (
     <div>
       <Header course={course} />
-      <Part 
-      name= "Fundamental  of React" 
-      exercises={10}
+      <Content 
+        part1="Fundamental  of React" 
+        exercises1={10}
+        part2="Using props to pass data"
+        exercises2={7}
+        part3="State of a component"
+        exercises3={14}
        />
-      <Part 
-      name = "Using props to pass data"
-      exercises={7} 
-      />
-      <Part 
-      name = "State of a component"
-      exercises={14} 
+      <Total 
+        exercises1={10}
+        exercises2={7}
+        exercises3={14}
       />
     </div>
   )
 }
+
+  
+
+
 export default App
+
+
+
