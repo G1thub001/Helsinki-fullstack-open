2668bdf99abc4f1ea6466171f02f779f40567c55
Brian <Bkibagendi20@gmail.com>
2026-07-30 07:07:33 -0700

    Exercise 1.4 - Refactor course parts into an array

--- Helsinki-fullstack-open/part1/src/App.tsx
+++ Helsinki-fullstack-open/part1/src/App.tsx
@@ -20,16 +20,14 @@ function Part({name, exercises}: PartProps) {
 }
 
 type ContentProps = {
-  part1: PartProps
-  part2: PartProps
-  part3: PartProps
+  parts: PartProps[]
 }
-function Content({part1, part2, part3}: ContentProps) {
+function Content({parts}: ContentProps) {
   return (
     <div>
-      <Part name={part1.name} exercises={part1.exercises} />
-      <Part name={part2.name} exercises={part2.exercises} />
-      <Part name={part3.name} exercises={part3.exercises} />
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
     </div>
   )
 }
@@ -49,31 +47,34 @@ function Total({exercises1, exercises2, exercises3}: TotalProps) {
 }
 
 function App() {
-  const course = "Half stack application development"
-  const part1={
-    name: "Fundamental of React",
-    exercises: 10
-  }
-  const part2={
-    name: "Using props to pass data",
-    exercises: 7
-  }
-  const part3={
-    name: "State of a component",
-    exercises: 14
-  }
+  
+  const course= "Half stack application development" 
+
+  const  parts= [
+    {
+      name: "Fundamental of React",
+      exercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7
+    },
+    {
+      name: "State of a component",
+      exercises: 14
+    }
+  ]
+  
   return (
     <div>
       <Header course={course} />
       <Content 
-        part1={part1} 
-        part2={part2}
-        part3={part3}
+       parts= {parts}
        />
       <Total 
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
+        exercises1={parts[0].exercises}
+        exercises2={parts[1].exercises}
+        exercises3={parts[2].exercises}
       />
     </div>
   )
@@ -81,8 +82,11 @@ function App() {
 
   
 
-
 export default App
+
+
+
+  
 
 
 
