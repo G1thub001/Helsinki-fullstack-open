410791db016ebef3ef7df4d642631612caef5eae
Brian <Bkibagendi20@gmail.com>
2026-08-07 04:16:29 -0700

    Complete Anecdotes random selection

--- Helsinki-fullstack-open/part1/src/App.tsx
+++ Helsinki-fullstack-open/part1/src/App.tsx
@@ -1,146 +1,61 @@
-import { useState } from "react"
+import {useState} from 'react'
 
 type HeadingProps = {
   heading: string
 }
 function Heading({heading}: HeadingProps) {
-
   return <h1>{heading}</h1>
-
 }
 
 type ButtonProps = {
- text: string
- onClick: () => void
+  text: string
+  handleClick: () => void
 }
-function Button({text, onClick}: ButtonProps) {
+function Button({text, handleClick}: ButtonProps) {
+
   return (
-    <button onClick={onClick}>
+    <button onClick={handleClick}>
       {text}
     </button>
   )
 }
 
-type FeedbackProps = {
-  onGood: () => void
-  onNeutral: () => void
-  onBad: () => void 
+function App() {
+  const [selected, setSelected] = useState(0)
+
+const anecdotes = [
+  'If it hurts, do it more often.',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+
+  const randomAnecdote = () => {
+   
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
+   
+  }
+
+  return (
+    <div>
+      <Heading 
+      heading="Anecdote of the day" 
+      />
+      <p>{anecdotes[selected]}</p>
+      <Button 
+      text="Next anecdote" handleClick={randomAnecdote}
+       />
+    </div>)
+
+  
   
 }
-function Feedback({onGood, onNeutral, onBad}: FeedbackProps) {
-   
-  return (
-    
-    <>
-      <Button 
-      text="good" 
-      onClick={onGood} 
-      />
-      <Button 
-      text="neutral" 
-      onClick={onNeutral}
-       />
-      <Button
-       text="bad" 
-       onClick={onBad}
-        />
-    </>
-  )
 
-
-}
-
-
-
-type StatisticsProps = {
- good: number
-  neutral: number
-  bad: number
-  all: number
-  average: number
-  positive: number
-}
-function Statistics({ good, neutral, bad, all, average, positive }: StatisticsProps) {
-  if (all === 0) {
-  return <p>No feedback given</p>
-}
-  return (
-     
-    <table>
-      <tbody>
-    
-    <StatisticLine text="Good" value={good} />
-    <StatisticLine text="Neutral" value={neutral} />
-    <StatisticLine text="Bad" value={bad} />
-    <StatisticLine text="All" value={all} />
-    <StatisticLine text="Average" value={average} />
-    <StatisticLine text="Positive" value={positive} />
-      </tbody>
-    </table>
-  )
-
-}
-
-type StatisticLineProps = {
-  text: string
-  value: number
-}
-function StatisticLine({text, value}: StatisticLineProps) {
-  return (
-    <tr>
-      <td> {text} </td>
-      <td> {value} </td>
-    </tr>
-  )
-}
-
-
-
-function App() {
-const [good, setGood] = useState(0)
-const [neutral, setNeutral] = useState(0)
-const [bad, setBad] = useState(0)
-
-const handleGood = () => {
-   setGood(good + 1)
-}
-const handleNeutral = () => {
-  setNeutral(neutral + 1)
-}
-const handleBad = () => {
-  setBad(bad + 1)
-}
-
-const all = good + neutral + bad
-const average = (good - bad) / all
-const positive = (good / all) * 100
-
-return (
-  <div>
-    <Heading 
-    heading= "give feedback"
-    />
-    
-   
-
-    <Feedback 
-      onGood={handleGood}
-      onNeutral={handleNeutral}
-      onBad={handleBad}
-    /> 
-
-    <Heading
-    heading= "statistics"
-    />
-    <Statistics
-     good={good}
-     neutral={neutral}
-     bad={bad}
-      all={all}
-      positive={positive}
-      average={average}
-    />
-  </div>
-)
-}
 export default App
+
+  
+
