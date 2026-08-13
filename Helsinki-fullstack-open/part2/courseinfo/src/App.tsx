68ecea4739fe9792b8af14aa263c7f1c9187bd94
Brian <Bkibagendi20@gmail.com>
2026-08-13 07:28:55 -0400

    Complete Part 2 exercises 2.4 and 2.5

--- Helsinki-fullstack-open/part2/courseinfo/src/App.tsx
+++ Helsinki-fullstack-open/part2/courseinfo/src/App.tsx
@@ -1,60 +1,6 @@
 
 
- type HeadingProps= {
-  heading: string
- }
- 
-function Header({heading}: HeadingProps) {
-  return <h1>{heading}</h1>
-
-}
-
-
-type Part = {
-  id: number
-  name: string
-  exercises: number
-}
-type PartProps = {
-  part: Part
-  }
-
-function Part({part}: PartProps) {
-return ( 
-  <h1>
-{part.name} {part.exercises}
-  </h1>
-)
-}
-
-type Course = {
-  name: string
-  parts: Part[]
-}
-type CourseProps = {
-  course: Course
-}
-function Course({course}: CourseProps) {
-  return (
-    <div>
-      <Header heading={course.name} />
-      {course.parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-      <Total parts={course.parts} />
-    </div>
-  )
- 
-}
-type TotalProps = {
-  parts: Part[]
-}
-function Total({ parts }: TotalProps) {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return (
-    <strong>  Total exercises: {total}</strong>
-  )
-}
+import Course from './Course'
 
 function App() {
   const courses = [
