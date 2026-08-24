604f497ded12fecb428ededaf6b44ad07fa6b9af
Brian <Bkibagendi20@gmail.com>
2026-08-24 08:35:43 -0400

    Complete country information exercise

--- Helsinki-fullstack-open/part2/courseinfo/src/App.tsx
+++ Helsinki-fullstack-open/part2/courseinfo/src/App.tsx
@@ -47,6 +47,9 @@ function App() {
     })
 }
 
+const showCountry = (country: Country) => {
+  setCountries([country])
+}
   return (
     <div>
       <form onSubmit={handleSearch}>
@@ -57,29 +60,44 @@ function App() {
         />
         <button type="submit">search</button>
       </form>
-
+      
+{countries.length > 10 && (
+  <p>Too many matches, specify another filter</p>
+)}
+{countries.length > 1 && countries.length <= 10 && (
+  <ul>
+    {countries.map(country => (
+      <li key={country.cca3}>
+        {country.name.common}
+        <button onClick={() => showCountry(country)}>
+          show
+        </button>
+      </li>
+    ))}
+  </ul>
+)}
 {countries.length === 1 && (
   <div>
     <h2>{countries[0].name.common}</h2>
 
-<img
-  src={countries[0].flags.png}
-  alt={`Flag of ${countries[0].name.common}`}
-/>
+    <img
+      src={countries[0].flags.png}
+      alt={`Flag of ${countries[0].name.common}`}
+    />
 
     <p>capital {countries[0].capital}</p>
     <p>area {countries[0].area}</p>
     <p>population {countries[0].population}</p>
 
     <h3>languages:</h3>
+
     <ul>
-  {Object.values(countries[0].languages).map(language => (
-    <li key={language}>{language}</li>
-  ))}
-</ul>
+      {Object.values(countries[0].languages).map(language => (
+        <li key={language}>{language}</li>
+      ))}
+    </ul>
   </div>
 )}
-
   </div>
   )
 }
