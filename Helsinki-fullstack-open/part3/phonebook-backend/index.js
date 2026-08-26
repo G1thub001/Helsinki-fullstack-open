280b464ccf8f6caa0fc4123a939d12927aa3952e
Brian <Bkibagendi20@gmail.com>
2026-08-26 06:17:16 -0400

    Complete part 3 exercise 3.8

--- Helsinki-fullstack-open/part3/phonebook-backend/index.js
+++ Helsinki-fullstack-open/part3/phonebook-backend/index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-app.use(morgan('tiny'))
 app.use(express.json())
+
+morgan.token('body', request => {
+  return JSON.stringify(request.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
+
+
 
 let persons = [
   {
