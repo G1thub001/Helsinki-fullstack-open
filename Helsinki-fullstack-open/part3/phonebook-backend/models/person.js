7cdbbea6bbbf1939a2ff90533de8cf1afbd12fdb
Brian <Bkibagendi20@gmail.com>
2026-08-28 07:55:46 -0400

    Add phone number validation

--- Helsinki-fullstack-open/part3/phonebook-backend/models/person.js
+++ Helsinki-fullstack-open/part3/phonebook-backend/models/person.js
@@ -6,7 +6,16 @@ const personSchema = new mongoose.Schema({
     minlength: 3,
     required: true
   },
-  number: String
+
+  number: {
+    type: String,
+    validate: {
+      validator: function(value) {
+        return /^\d{2,3}-\d{7,}$/.test(value)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
