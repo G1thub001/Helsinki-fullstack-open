0483c174908f2d9462e6c27f3dd404d18f469f54
Brian <Bkibagendi20@gmail.com>
2026-08-31 07:51:32 -0400

    Add totalLikes helper and test

--- Helsinki-fullstack-open/part4/bloglist-backend/utils/list_helper.test.js
+++ Helsinki-fullstack-open/part4/bloglist-backend/utils/list_helper.test.js
@@ -9,3 +9,30 @@ test('dummy returns one', () => {
 
   assert.strictEqual(result, 1)
 })
+
+test('total likes', () => {
+  const blogs = [
+    {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5
+    },
+    {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12
+    },
+    {
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'https://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+      likes: 3
+    }
+  ]
+
+  const result = listHelper.totalLikes(blogs)
+
+  assert.strictEqual(result, 20)
+})
