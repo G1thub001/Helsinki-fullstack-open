a3c6f06cffabc2c16b14d1e97e8fd576769cfe6b
Brian <Bkibagendi20@gmail.com>
2026-08-31 08:05:16 -0400

    Add favoriteBlog helper and test

--- Helsinki-fullstack-open/part4/bloglist-backend/utils/list_helper.test.js
+++ Helsinki-fullstack-open/part4/bloglist-backend/utils/list_helper.test.js
@@ -36,3 +36,30 @@ test('total likes', () => {
 
   assert.strictEqual(result, 20)
 })
+
+test('favorite blog', () => {
+  const blogs = [
+    {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5
+    },
+    {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12
+    },
+    {
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'https://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+      likes: 3
+    }
+  ]
+
+  const result = listHelper.favoriteBlog(blogs)
+
+  assert.deepStrictEqual(result, blogs[1])
+})
