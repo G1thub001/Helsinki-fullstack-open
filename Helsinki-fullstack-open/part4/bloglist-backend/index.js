48e9db4e676088a404746af7dd90f995c5a00696
Brian <Bkibagendi20@gmail.com>
2026-09-01 06:42:44 -0400

    Add backend integration testing setup

--- Helsinki-fullstack-open/part4/bloglist-backend/index.js
+++ Helsinki-fullstack-open/part4/bloglist-backend/index.js
@@ -1,16 +1,10 @@
 const config = require('./utils/config')
-
-const express = require('express')
 const mongoose = require('mongoose')
 const dns = require('dns')
-const Blog = require('./models/blog')
-const blogRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
+const app = require('./app')
 
 dns.setServers(['8.8.8.8', '1.1.1.1'])
-
-const app = express()
 
 const url = config.MONGODB_URI
 
@@ -22,14 +16,8 @@ mongoose.connect(url)
     logger.error('error connecting to MongoDB:', error.message)
   })
 
-app.use(express.json())
-app.use('/api/blogs', blogRouter)
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-
 const PORT = config.PORT
+
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
-
