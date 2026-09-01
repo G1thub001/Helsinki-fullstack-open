d68964c42aeb61ac23f74a2a4b551390055df422
Brian <Bkibagendi20@gmail.com>
2026-09-01 07:28:45 -0400

    Add backend API tests

--- Helsinki-fullstack-open/part4/bloglist-backend/tests/blogs_api.test.js
+++ Helsinki-fullstack-open/part4/bloglist-backend/tests/blogs_api.test.js
@@ -5,18 +5,86 @@ const mongoose = require('mongoose')
 const dns = require('dns')
 const config = require('../utils/config')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 dns.setServers(['8.8.8.8', '1.1.1.1'])
 
 const api = supertest(app)
 
+const initialBlogs = [
+  {
+    title: 'HTML is easy',
+    author: 'John Doe',
+    url: 'https://example.com/html',
+    likes: 5
+  },
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7
+  }
+]
+
+test.beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
 mongoose.connect(config.MONGODB_URI)
 
 test('blogs are returned as json', async () => {
-  await api
+  const response = await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.length, initialBlogs.length)
+
+  const titles = response.body.map(blog => blog.title)
+
+  assert(titles.includes('HTML is easy'))
+  assert(titles.includes('React patterns'))
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'A new blog',
+    author: 'John Doe',
+    url: 'https://example.com/new-blog',
+    likes: 10
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, initialBlogs.length + 1)
+
+  const titles = response.body.map(blog => blog.title)
+
+  assert(titles.includes('A new blog'))
+})
+
+test('a blog without a title is not added', async () => {
+  const newBlog = {
+    author: 'John Doe',
+    url: 'https://example.com/no-title',
+    likes: 10
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
 test.after(async () => {
--- Helsinki-fullstack-open/part4/bloglist-backend/utils/middleware.js
+++ Helsinki-fullstack-open/part4/bloglist-backend/utils/middleware.js
@@ -1,12 +1,16 @@
+const logger = require('./logger')
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 const errorHandler = (error, request, response, next) => {
-  logger.log(error.message)
-
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
   }
 
   next(error)
